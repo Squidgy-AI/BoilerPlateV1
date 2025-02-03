09dd841a6dcee6f61d4d31ddb2322370a1252932
R-Mohnish-Kumar <mohnish694@gmail.com>
2025-02-03 15:34:52 +0000

    Environment Update

---
+++ GHL_Revamp_Dev/environment/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    appointment_url : "https://services.leadconnectorhq.com/calendars/events/appointments/",
+    sub_acc_url : "https://services.leadconnectorhq.com/locations/",
+    calendars_url : "https://services.leadconnectorhq.com/calendars/",
+    users_url : "https://services.leadconnectorhq.com/users/",
+    contacts_url : "https://services.leadconnectorhq.com/contacts/",
+}
